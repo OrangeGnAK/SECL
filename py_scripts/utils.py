Fixed bugs in mean & std calculation function and added NaN filter to it

py_scripts/utils.py
@@ -18,22 +18,27 @@ def mean_std_calc(dataset, batch_size=16, num_workers=4):
         pin_memory=True
     )
     
-    first_batch = next(iter(loader))
-    num_channels = first_batch.shape 
+    first_batch = next(iter(loader))[0]
+    num_channels = first_batch.shape[1] 
 
     channels_sum = torch.zeros(num_channels, dtype=torch.float64, device=device)
     channels_sq_sum = torch.zeros(num_channels, dtype=torch.float64, device=device)
-    total_pixels = 0.0
+    total_pixels = torch.zeros(num_channels, dtype=torch.float64, device=device)
 
     for batch in tqdm(loader):
-        images = batch.to(torch.float64).to(device)
+        images = batch[0].to(torch.float64).to(device)
+
+        valid_mask = torch.isfinite(images)
+        
+        images = torch.nan_to_num(images, nan=0.0, posinf=0.0, neginf=0.0)
         
         # [B, C, H, W] -> [C, B * H * W]
         images = images.transpose(0, 1).flatten(1)
+        valid_mask = valid_mask.transpose(0, 1).flatten(1)
         
         channels_sum += images.sum(dim=1)
         channels_sq_sum += (images ** 2).sum(dim=1)
-        total_pixels += images.shape
+        total_pixels += valid_mask.sum(dim=1).to(torch.float64)
 
     mean = channels_sum / total_pixels
     var = (channels_sq_sum / total_pixels) - (mean ** 2)